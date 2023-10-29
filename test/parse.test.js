83f57d439487ff4ddad7d2b3691c3e0f57504b9e
Ian Holmes <ihholmes@gmail.com>
2023-10-29 11:26:54 -0700

    refined tests

--- test/parse.test.js
+++ test/parse.test.js
@@ -5,53 +5,50 @@ import { makeGrammarIndex, expandInherits, compileTypes, parseOrUndefined, gramm
 import { serialize } from '../src/serialize.js';
 import { stringify } from '../src/canonical-json.js';
 
-const testParse = (grammarText, expectedSerialization) => {
-    let rules = parseOrUndefined (grammarText, false);
-    if (expectedSerialization===false)
-        expect(rules).to.be.undefined;
-    else
-        expect(serialize(rules)).to.equal(expectedSerialization || grammarText);
-}
-
-const testInherit = (grammarText, expectedSerialization) => {
-    let rules = parseOrUndefined (grammarText, false);
-    if (expectedSerialization===false)
-        expect(rules).to.be.undefined;
-    else {
-        rules = grammarIndexToRuleList (expandInherits (makeGrammarIndex (rules)));
-        expect(serialize(rules)).to.equal(expectedSerialization || grammarText);
+const testParse = (grammarText, opts) => {
+    opts = opts || {};
+    let consoleErrorFunc, consoleErrorText = '';
+    if (opts.error) {
+        consoleErrorFunc = console.error;
+        console.error = function() { consoleErrorText += Array.from(arguments).map((arg)=>arg.toString()).join('') }
     }
-}
-
-const testCompile = (grammarText, expectedSerialization) => {
     let rules = parseOrUndefined (grammarText, false);
-    if (expectedSerialization===false)
+    if (opts.error) {
+        console.error = consoleErrorFunc;
         expect(rules).to.be.undefined;
-    else {
-        rules = compiledGrammarIndexToRuleList (compileTypes (rules));
-        expect(serialize(rules)).to.equal(expectedSerialization || grammarText);
+        if (typeof(opts.error)==='string')
+            expect(consoleErrorText).to.equal(opts.error);
+    } else {
+        expect(rules).to.not.be.undefined;
+        let expectedSerialization = opts.expect || grammarText;
+        if (!opts.literal)
+            expectedSerialization = expectedSerialization.replace(/\.\s*/g, '.\n');
+        if (opts.compile)
+            rules = compiledGrammarIndexToRuleList (compileTypes (rules));
+        else if (opts.inherit)
+            rules = grammarIndexToRuleList (expandInherits (makeGrammarIndex (rules)));
+        expect(serialize(rules)).to.equal(expectedSerialization);
     }
 }
 
 describe ('Testing the parser', () => {
     it ('Basic rule parsing', (done) => {
-        testParse ('a b : c d.\n');
-        testParse ('a b : c d.', 'a b : c d.\n');
-        testParse ('a b:c d.', 'a b : c d.\n');
-        testParse ('a b : c d', 'a b : c d.\n');
-        testParse ('a b :', false);
+        testParse ('a b : c d.\n',{literal:true});
+        testParse ('a b : c d.', {expect:'a b : c d.\n',literal:true});
+        testParse ('a b:c d.', {expect:'a b : c d.'});
+        testParse ('a b : c d', {expect:'a b : c d.'});
+        testParse ('a b :', {error:true});
         done();
     })
 
     it ('Multi-rule parsing', (done) => {
-        testParse ('a b : c d.\ne : f.\n');
-        testParse ('a b : c d. e : f.', 'a b : c d.\ne : f.\n');
+        testParse ('a b : c d. e : f.');
         done();
     })
 
     it ('Inheritance', (done) => {
-        testInherit ('a b : c d. e = a.', 'a b : c d.\ne b : c d.\n');
-        testParse ('a = b. b = a.', false);
+        testParse ('a b : c d. e = a.', {inherit:true,expect:'a b : c d. e b : c d.'});
+        testParse ('a = b. b = a.', {error:"Type 'a' inherits from itself"});
         done();
     })
 })
