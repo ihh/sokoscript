89afcfa316b8b385b5dc39bf983a218880d308bc
Ian Holmes <ihholmes@gmail.com>
2023-11-05 13:14:37 -0500

    inching towards it

--- lambda/boards.js
+++ lambda/boards.js
@@ -4,8 +4,16 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 // Global parameters
 const maxOutgoingMovesForBlock = 100;
 const maxTimeInSecondsBetweenBlocks = 600;
+
+const maxMoveAnticipationMillisecs = 250;
+const maxMoveDelayMillisecs = 250;
+
 const blockTicksPerSecond = BigInt(Math.pow(2,32));
 const maxTicksBetweenBlocks = BigInt(maxTimeInSecondsBetweenBlocks) * blockTicksPerSecond;
+
+
+
+const MaxMoveRetries = 3;
 
 const userTableName = 'soko-users';
 const clockTableName = 'soko-clocks';
@@ -113,9 +121,35 @@ const handler = async (event) => {
     switch (routeKey) {
         case 'POST /boards':
             // create a random board ID
+            const id = Math.floor(Math.random()*Math.pow(2,32)).toString(32)
             // create a clock table entry with this board ID, owned by caller, with lastMoveTime=now, conditional on none existing
+            const clockUpdateParams = {
+                TableName: clockTableName,
+                Key: { boardId: id },
+                ConditionExpression: 'attribute_not_exists(boardId)',
+                UpdateExpression: 'set owner=:owner, lastMoveTime=:lastMoveTime',
+                ExpressionAttributeValues: {
+                    ':owner': event.requestContext.identity.cognitoIdentityId,
+                    ':lastMoveTime': Date.now()
+                }
+            };
+            let clockUpdate = dynamoDB.update(clockUpdateParams).promise();
+            let clockResult = await clockUpdate;
+            if (!clockResult)
+                throw new Error ('Clock update failed');
             // create an initial entry in the block table
+            const blockUpdateParams = {
+                // TODO: write me
+            };
+            let blockUpdate = dynamoDB.update(blockUpdateParams).promise();
+            let blockResult = await blockUpdate;
+            if (!(blockResult?.Items?.length === 1))
+                throw new Error ('Block update failed');
             // return the board ID
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ id }),
+            };
             break;
 
         case 'GET /boards':
@@ -125,17 +159,80 @@ const handler = async (event) => {
 
         case 'DELETE /boards/{id}':
             // delete the clock table entry for this board ID, conditional on owner matching caller ID
+            // if successful: delete all block and move table entries for this board ID
             break;
 
-        case 'POST /boards/{id}/moves':
+        case 'POST /boards/{id}/moves': {
+            // move time is specified in milliseconds since epoch
+            const requestedUnixTime = parseInt (event.body?.time);
+            const originallyRequestedUnixTime = requestedTime;
+            let requestedBoardTime = BigInt(requestedTime) * blockTicksPerSecond / 1000n;
             // TODO: verify that move fits JSON schema for a move
-            // check that requested time is not out of bounds
-            // Retry up to rnd(3)+1 times:
-            //  - get clock table entry for board. Check permissions. Set requestedTime=max(requestedTime,lastMoveTime+1)
-            //  - update clock table with requested time, conditional on lastMoveTime=lastMoveTimeAsRetrieved
-            // if clock table update succeeded, create a move table entry with this board ID and requestedTime, attributed to caller
-            // return the move time
-            break;
+            // Retry up to rnd(MaxMoveRetries) times:
+            const moveRetries = Math.floor(Math.random()*MaxMoveRetries);
+            let retry = 0, newClock = false, newMove = false;
+            for (retry = 0; retry <= moveRetries && !newClock; ++retry) {
+                const currentTimeOnServer = Date.now();
+                //  - check that originallyRequestedTime - maxMoveAnticipation <= currentTimeOnServer <= originallyRequestedTime + maxMoveDelay
+                //    if it isn't, return an error
+                if (!(originallyRequestedUnixTime - maxMoveAnticipationMillisecs <= currentTimeOnServer <= originallyRequestedUnixTime + maxMoveDelayMillisecs))
+                    return {
+                        statusCode: 400,
+                        body: JSON.stringify({ message: 'Move time out of range' }),
+                    };
+                //  - retrieve clock table entry for board. Check permissions. Set requestedTime=max(requestedTime,lastMoveTimeRetrieved+1)
+                let clock = await getClockTableEntry (event.pathParameters?.id);
+                // TODO: check permissions
+                requestedBoardTime = Math.max (requestedBoardTime, clock.lastMoveTime + 1);
+                //  - update clock table with requested time, conditional on lastMoveTime=lastMoveTimeRetrieved
+                const clockUpdateParams = {
+                    TableName: clockTableName,
+                    Key: { boardId: event.pathParameters?.id },
+                    ConditionExpression: 'lastMoveTime = :lastMoveTime',
+                    UpdateExpression: 'set lastMoveTime=:requestedTime',
+                    ExpressionAttributeValues: {
+                        ':requestedTime': requestedBoardTime.toString(),
+                        ':lastMoveTime': clock.lastMoveTime.toString()
+                    }
+                };
+                clockUpdate = await dynamoDB.update(clockUpdateParams).promise();
+                if (clockUpdate?.Items?.length === 1)
+                    newClock = clockUpdate.Items[0];
+            }
+            // if clock table update succeeded...
+            if (newClock) {
+                //  - create a move table entry with this board ID, requestedTime, and originallyRequestedTime, attributed to caller
+                const moveUpdateParams = {
+                    TableName: moveTableName,
+                    Key: { boardId: event.pathParameters?.id, moveTime: requestedBoardTime.toString() },
+                    ConditionExpression: 'attribute_not_exists(boardId)',
+                    UpdateExpression: 'set owner=:owner, originallyRequestedTime=:originallyRequestedTime',
+                    ExpressionAttributeValues: {
+                        ':owner': event.requestContext.identity.cognitoIdentityId,
+                        ':originallyRequestedTime': originallyRequestedBoardTime.toString()
+                    }
+                };
+                while (!newMove) {
+                    //  - retry persistently until move table entry is created
+                    const moveUpdate = await dynamoDB.update(moveUpdateParams).promise();
+                    if (moveUpdate?.Items?.length === 1)
+                        newMove = moveUpdate.Items[0];
+                }
+            }
+
+            if (newMove)
+                // return the move time
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ boardTime: requestedBoardTime.toString() }),
+                };
+
+            // if move table entry creation failed, return an error
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: 'Move creation failed' }),
+            };
+        }
     
         case 'GET /boards/{id}/blocks/{hash}': {
             const block = await getBlockAndOutgoingMoves (event.pathParameters?.id, event.pathParameters?.hash, event.queryParameters?.headerOnly);
@@ -150,8 +247,7 @@ const handler = async (event) => {
             // get clock table entry for board, and verify that board size matches
             // get previous block from block table, and its outgoing moves
             // verify that move list is complete, and that its hash matches the update
-            // verify that previous block and update block have consistent timestamps
-            // verify that caller has not posted or confirmed any other blocks as a successor to the previous block
+            // verify that previousBlockTime < updateBlockTime <= previousBlockTime + maxTimeBetweenBlocks
             // if all checks pass, create a block table entry with this board ID and hash, attributed to caller, conditional on none existing
             // if the block already existed, update the existing entry to include the caller as one of the confirmers
             // return success or failure
